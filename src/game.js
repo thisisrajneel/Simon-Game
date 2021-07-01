4d3faa3498456ca1a47eae2e6ece5d7f5edf3175
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-07-01 16:22:36 +0530

    modified, put code inside nextSequence

--- src/game.js
+++ src/game.js
@@ -1,39 +1,13 @@
 var buttoncolours = ["red","blue","green","yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 const nextSequence = () => {
-    var randomNumber = Math.floor(Math.random()*4);
-}
-
-var randomChosenColour = buttoncolours[randomNumber];
-gamePattern.push(randomChosenColour);
-
-setTimeout(() => {
-    $("#"+randomChosenColour).fadeOut(100);
-    $("#"+randomChosenColour).fadeIn(100);
-}, 500);
-
-$("btn").click(() => {
-    alert('hello');
-});
-
-switch (randomChosenColour) {
-    case "red":
-        var sound = new Audio('sounds/' + randomChosenColour);
-    break;
     
-    case "yellow":
-        var sound = new Audio('sounds/' + randomChosenColour);
-    break;
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttoncolours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+    var sound = new Audio('sounds/' + randomChosenColour);
+    sound.play();
 
-    case "green":
-        var sound = new Audio('sounds/' + randomChosenColour);
-    break;
-
-    case "blue":
-        var sound = new Audio('sounds/' + randomChosenColour);
-    break;
-
-    default:
-        var sound = new Audio('sounds/wrong.mp3');
-    break;
 }
