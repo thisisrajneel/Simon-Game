101ea4c1658b646712295b0e6e2a97b8e8912380
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-07-01 16:52:51 +0530

    changed arrow fn to regular fn in click method

--- src/game.js
+++ src/game.js
@@ -1,10 +1,17 @@
-var buttoncolours = ["red","blue","green","yellow"];
+var buttonColours = ["red","blue","green","yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+
+$(".btn").click( function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+});
+
 const nextSequence = () => {
     
     var randomNumber = Math.floor(Math.random()*4);
-    var randomChosenColour = buttoncolours[randomNumber];
+    var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
     var sound = new Audio('sounds/' + randomChosenColour);
