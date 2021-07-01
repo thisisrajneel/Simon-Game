5e2faa56336e41c79c36d246f40c2e97da444657
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-07-01 17:03:35 +0530

    added button sounds

--- src/game.js
+++ src/game.js
@@ -6,15 +6,19 @@ $(".btn").click( function() {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
     console.log(userClickedPattern);
+    playSound(userChosenColour);
 });
 
-const nextSequence = () => {
+function nextSequence () {
     
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
     $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
-    var sound = new Audio('sounds/' + randomChosenColour);
-    sound.play();
+    playSound(randomChosenColour);
+}
 
+function playSound (name) {
+    var sound = new Audio('sounds/' + name + ".mp3");
+    sound.play();
 }
